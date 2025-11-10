0bac480ccbcbaf224c89afe5660aabc3e4a7fea0
royeiror <royeiror@gmail.com>
2025-11-10 12:12:07 -0600

    Update viewer.cpp

--- gui/viewer.cpp
+++ gui/viewer.cpp
@@ -1,1 +1,43 @@
+#include "viewer.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+Viewer::Viewer() {}
+
+void Viewer::init() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "FabSim Viewer", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+
+    points = { {0,0,0}, {1,0,0}, {0,1,0} };
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, points.size()*3*sizeof(float), points.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glEnableVertexAttribArray(0);
+
+    while(!glfwWindowShouldClose(window)) {
+        render();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+}
+
+void Viewer::render() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void Viewer::run() {
+    init();
+}
 
