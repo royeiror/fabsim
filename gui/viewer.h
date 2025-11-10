b186a85d6ba976e547d637857984a72a894d4b81
royeiror <royeiror@gmail.com>
2025-11-10 12:11:47 -0600

    Update viewer.h

--- gui/viewer.h
+++ gui/viewer.h
@@ -1,1 +1,15 @@
 
+#pragma once
+#include <vector>
+#include <Eigen/Core>
+
+class Viewer {
+public:
+    Viewer();
+    void run();
+private:
+    void init();
+    void render();
+    unsigned int vao = 0, vbo = 0;
+    std::vector<Eigen::Vector3f> points;
+};
